782e59ebd7beb4c4b4dc17e6e3fd32e9229e6d86
Yahaya Azeez <azconcept2016@gmail.com>
2024-07-24 22:42:09 +0100

    add resolve and reject into promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,5 @@
 export default function signUpUser(firstName, lastName) {
-    return new Promise.resolve({ firstName, lastName });
+    return new Promise((resolve, reject) => {
+        resolve({ firstName, lastName });
+    });
 }
