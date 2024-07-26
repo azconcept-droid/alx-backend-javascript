fc9924b079bacec0ecd361ebd6144d6449a26e01
Yahaya Azeez <azconcept2016@gmail.com>
2024-07-26 23:20:39 +0100

    get obj length with Object.keys()

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -3,10 +3,7 @@ export default function createReportObject(employeesList) {
     const report = {
         allEmployees: employeesList,
         getNumberOfDepartments: (employeesList) => {
-            let count = 0
-            for (const departmentName in employeesList) {
-                count++;
-            }
+            let count = Object.keys(employeesList).length;
 
             return count;
         }
