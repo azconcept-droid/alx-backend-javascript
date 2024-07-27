aa6b3107ab663846daef967bab3d19e9b82222bc
Yahaya Azeez <azconcept2016@gmail.com>
2024-07-27 05:42:04 +0100

    fix for loop Iteration bug

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,11 @@
 export default function createIteratorObject(report) {
     let allEmployees = report.allEmployees
     let employees = Object.values(allEmployees);
-
     let arr = new Array()
-    for (let i = 0; i < report.getNumberOfDepartments(allEmployees); i += 1) {
-        arr.push(...employees)
+    
+    for (const item of employees) {
+        arr.push(item)
     }
+
     return arr;
 }
