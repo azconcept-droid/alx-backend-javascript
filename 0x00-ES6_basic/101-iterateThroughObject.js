24f68f7472743a441620522be3c1c46dbcecafe3
Yahaya Azeez <azconcept2016@gmail.com>
2024-07-27 05:35:08 +0100

    diplay string of employees name

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,9 @@
+export default function iterateThroughObject(reportWithIterator) {
+    let displayItem = '';
+
+    for (const item of reportWithIterator) {
+        displayItem += item
+    }
+
+    return displayItem;
+}
