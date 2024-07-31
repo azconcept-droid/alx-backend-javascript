c30aceeb81aff6c1f5525d12d626a94c4e1f9646
Yahaya Azeez Adebayo <azconcept2016@gmail.com>
2024-08-01 00:57:41 +0100

    make attribute optional

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,7 +4,7 @@ interface Teacher {
     fullTimeEmployee: boolean;
     yearsOfExperience: number;
     location: string;
-    contract: true | false;
+    contract?: boolean;
 }
 
 interface Directors extends Teacher {
