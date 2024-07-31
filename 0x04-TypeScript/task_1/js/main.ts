ffd0a857e1efd0491684f1997b90d249da61ab8c
Yahaya Azeez <azconcept2016@gmail.com>
2024-07-31 15:24:02 +0100

    add student class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,21 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
     console.log(`${firstName[0]}. ${lastName}`);
 };
+
+class StudentClass {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working"
+    }
+
+    displayName (): string {
+        return this.firstName
+    }
+}
