9cbd06e41ea270ebe7f905a3062108f6beb2d3e2
Yahaya Azeez <azconcept2016@gmail.com>
2024-09-26 22:32:18 +0100

    just push code :poop:

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,12 @@
+const sendPaymentRequestToApi = require("./3-payment")
+const sinon = require("sinon");
+const {expect} = require("chai");
+
+describe('sendPaymentRequestApi', function () {
+	describe('Test result', function () {
+
+        it('should return 120', function () {
+        expect().to.equal(120);
+        });
+    });
+});
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,29 @@
+const Utils = {
+    calculateNumber: function (type, a, b) {
+        let result;
+        let rounded_a = Math.round(a);
+        let rounded_b = Math.round(b);
+    
+        switch (type) {
+            case "SUM":
+                result = rounded_a + rounded_b;
+                break;
+            case "SUBTRACT":
+                result = rounded_a - rounded_b;
+                break
+            case "DIVIDE":
+                if (rounded_b === 0) {
+                    return "Error";
+                }
+                result = rounded_a / rounded_b;
+                break
+            default:
+                break;
+        }
+    
+        return result
+    }
+}
+
+// Export the Utils module
+module.exports = Utils;
