21aa528fafda99c8edd4dd82d86f2637818e22b4
Yahaya Azeez <azconcept2016@gmail.com>
2024-09-26 18:34:12 +0100

    add test cases for math operators

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,29 @@
+const calculateNumber = require('./1-calcul');
+var assert = require('assert');
+
+describe('CalculateNumber', function () {
+	describe('Test addition', function () {
+    it('should return 6', function () {
+      assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+
+	describe('Test subtraction', function () {
+    it('should not return -4', function () {
+      assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+  });
+
+	describe('Test division', function () {
+    it('should return 0.2', function () {
+      assert.equal(calculateNumber('DIVIDE',1.4, 4.5), 0.2);
+    });
+  });
+
+  describe('Test division infinite error', function () {
+    it('should return error', function () {
+      assert.equal(calculateNumber('DIVIDE', 1.4, 0), "Error");
+    });
+  });
+
+})
