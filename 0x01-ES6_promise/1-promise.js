40f4725e4bd18b2104e0184ff27818df21a92266
Yahaya Azeez <azconcept2016@gmail.com>
2024-11-15 10:48:27 +0100

    fix(bug): change lowercase to upper

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      const response = { status: 200, body: 'success' };
+      const response = { status: 200, body: 'Success' };
       resolve(response);
     } else {
       reject(new Error('The fake API is not working currently'));
