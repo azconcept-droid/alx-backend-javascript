f525e948ac6a7e1ae4b38f4c47192f5bc303a521
Yahaya Azeez <azconcept2016@gmail.com>
2024-11-15 19:43:02 +0100

    refactor code :recycle:

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,12 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    signUpUser(firstName, lastName);
-    uploadPhoto(fileName);
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((results) => {
+      results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      }));
+    });
 }
