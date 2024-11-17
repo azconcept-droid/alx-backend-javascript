8411e2c1603cec4ff847add04b1aa66c642adda1
Yahaya Azeez <azconcept2016@gmail.com>
2024-11-17 10:06:58 +0100

    refactor code

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,9 +1,3 @@
 export default function returnHowManyArguments(...theArgs) {
-  let counter = 0;
-
-  for (const arg of theArgs) {
-    counter += 1;
-  }
-
-  return counter;
+  return theArgs.length;
 }
