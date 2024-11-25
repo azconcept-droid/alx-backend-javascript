96fa282abd50fbd064ff6ddc5b7480be1f809095
Yahaya Azeez <azconcept2016@gmail.com>
2024-11-25 11:11:05 +0100

    fix: refactor code :recycle:

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,12 @@
-// Import the 'readline' module to handle input from the command line
-const readline = require('readline');
-
-// Create an interface for input and output
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-// Ask for the user's name
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  // Display the user's name
-  console.log(`Your name is: ${name}`);
-});
-
-// Handle the event when the user closes the program
-rl.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
