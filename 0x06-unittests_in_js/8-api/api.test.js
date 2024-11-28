e4ba9418b68b50e47de9559ad8f59d14b50e8837
Yahaya Azeez <azconcept2016@gmail.com>
2024-11-28 15:46:32 +0100

    add done() for async function

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,21 +2,24 @@ const { expect } = require("chai");
 const request = require("request");
 
 describe("Test Get Index page / endpoint", () => {
-  it("Correct status code 200", () => {
+  it("Correct status code 200", (done) => {
     request("http://localhost:7865/", (err, res, body) => {
       expect(res.statusCode).to.equal(200)
+      done()
     })
   });
 
-  it("Correct status code 404", () => {
+  it("Correct status code 404", (done) => {
     request("http://localhost:7865/*", (err, res, body) => {
       expect(res.statusCode).to.equal(404)
+      done()
     })
   });
 
-  it("Correct result", () => {
+  it("Correct result", (done) => {
     request("http://localhost:7865/", (err, res, body) => {
       expect(body).to.equal("Welcome to the payment system")
+      done()
     })
   })
 });
