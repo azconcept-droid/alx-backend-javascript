11f6e78a34e534b9b8df218e68c52d64e47c79fd
Yahaya Azeez <azconcept2016@gmail.com>
2025-02-23 15:56:59 +0100

    check set and startString

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || !startString) {
+    return ''
+  }
+
   const newString = [];
   set.forEach((element) => {
     if (element.startsWith(startString) && startString !== '') {
