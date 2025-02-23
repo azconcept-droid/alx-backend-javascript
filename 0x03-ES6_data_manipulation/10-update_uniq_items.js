4d288b66b9acce555457e47d518e5bfaeb40b406
Yahaya Azeez <azconcept2016@gmail.com>
2025-02-23 18:21:39 +0100

    fix eslint format

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,5 @@
 export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) throw new Error("Cannot process");
+  if (!(map instanceof Map)) throw new Error('Cannot process');
 
   for (const member of map) {
     if (member[1] === 1) map.set(member[0], 100);
