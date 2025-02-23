a2d20387fac2b8537b67a6e53b48d84ea8eefdd4
Yahaya Azeez <azconcept2016@gmail.com>
2025-02-23 18:18:42 +0100

    check instance of map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,6 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw new Error("Cannot process");
+
   for (const member of map) {
     if (member[1] === 1) map.set(member[0], 100);
   }
