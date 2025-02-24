6914c3025637977d550a6bca80769a1fa2b09f34
Yahaya Azeez <azconcept2016@gmail.com>
2025-02-24 12:50:04 +0100

    add test cases for array set

---
+++ 0x03-ES6_data_manipulation/tests/7-has_array_values.test.js
@@ -0,0 +1,15 @@
+import hasValuesFromArray from "../7-has_array_values.js";
+
+test("Create a set using an array of elements. Pass an array containing an element that isn't in resultant set and check if it returns false", () => {
+  const array = [1, 2, 'test'];
+  const set = new Set(array);
+  const array2 = [1, 2, 'test', 'banana'];
+  expect(hasValuesFromArray(set, array2)).toBe(false);
+});
+
+test("hasValuesFromArray contains all elements from an array passed into a set", () => {
+  const array = [1, 2, 'test'];
+  const set = new Set(array);
+
+  expect(hasValuesFromArray(set, array)).toBe(true);
+});
