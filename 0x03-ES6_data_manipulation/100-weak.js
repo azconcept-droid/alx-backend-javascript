38d303e4e7f0a2ec6aaefec7395851636fc1401a
Yahaya Azeez <azconcept2016@gmail.com>
2025-02-24 12:40:16 +0100

    change overload condition

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -5,9 +5,9 @@ const count = {};
 weakMap.set(count, 0);
 
 export function queryAPI(endpoint) {
-  if (weakMap.get(count) >= 5) throw new Error('Endpoint load is high');
-
   weakMap.set(count, weakMap.get(count) + 1);
 
   weakMap.set(endpoint, weakMap.get(count));
+
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
 }
