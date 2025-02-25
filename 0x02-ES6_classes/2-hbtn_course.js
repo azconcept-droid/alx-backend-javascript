e98dbc6830b013babb40c5d4c4ca4685f6df0cb1
Yahaya Azeez <azconcept2016@gmail.com>
2025-02-25 16:04:29 +0100

    set and get data in js classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class ALXCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    }
+    if (Array.isArray(students)) {
+      this._students = students;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  set length(len) {
+    this._length = len;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
+}
