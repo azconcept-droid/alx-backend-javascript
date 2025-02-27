8de29458f916ee1fc6ecea2a9844d162a26f0a7d
Yahaya Azeez <azconcept2016@gmail.com>
2025-02-27 17:55:20 +0100

    fix logic bug

--- 0x02-ES6_classes/main/6-main.js
+++ 0x02-ES6_classes/main/6-main.js
@@ -3,4 +3,4 @@ import SkyHighBuilding from '../6-sky_high.js';
 const building = new SkyHighBuilding(140, 60);
 console.log(building.sqft);
 console.log(building.floors);
-// console.log(building.evacuationWarningMessage());
+console.log(building.evacuationWarningMessage());
